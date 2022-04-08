75c123ea6212c4bbfda5619c0f77954be375750e
Lívia Renata <74880716+liviarenata@users.noreply.github.com>
2022-04-08 00:47:59 +0000

    ok

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 from flask import Flask, render_template, request, redirect
+
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask('app')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100))
+  email = db.Column(db.String())
+  password = db.Column(db.String())
+  created_at = db.Column(db.String())
+  updated_at = db.Column(db.String())
+
+class Contacts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String())
+    phone = db.Column(db.String())
+    image = db.Column(db.String())
+    user_id = db.Column(db.Integer)
+    created_at = db.Column(db.String())
+    updated_at = db.Column(db.String())
 
 todos = [ 
     {'title': 'João da Silva'},
@@ -20,4 +43,5 @@ def contats():
     return redirect('/')
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='0.0.0.0', port=8080)
