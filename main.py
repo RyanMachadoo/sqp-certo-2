02e54cfd06032cdb9613a1e64051cb2c11be77d2
Lívia Renata <74880716+liviarenata@users.noreply.github.com>
2022-05-14 19:01:24 +0000

    certo

--- main.py
+++ main.py
@@ -1,47 +1,48 @@
-from flask import Flask, render_template, request, redirect
 
+from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask('app')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
-class Users(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100))
-  email = db.Column(db.String())
-  password = db.Column(db.String())
-  created_at = db.Column(db.String())
-  updated_at = db.Column(db.String())
-
-class Contacts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    email = db.Column(db.String())
-    phone = db.Column(db.String())
-    image = db.Column(db.String())
-    user_id = db.Column(db.Integer)
-    created_at = db.Column(db.String())
-    updated_at = db.Column(db.String())
-
-todos = [ 
-    {'title': 'João da Silva'},
-    {'title':'Maria Souza'}
-]
+contacts = []
 
 @app.route('/')
 def index():
   return render_template(
-      'index.html',
-      todos = todos
+    'index.html',
+    contacts=contacts
   )
 
-@app.route('/contats', methods=['POST'])
-def contats():
-    title = request.form.get('title')
-    todos.append({'title':title})
-    return redirect('/')
+@app.route('/create', methods=['POST'])
+def create():
+  name = request.form.get('name')
+  email = request.form.get('email')
+  phone = request.form.get('phone')
+  contacts.append({
+    'name': name,
+    'email': email,
+    'phone': phone,
+  })
+  return redirect('/')
 
+@app.route('/delete/<int:index>')
+def delete(index):
+  contacts.pop(index)
+  return redirect('/')
+
+@app.route('/update/<int:index>', methods=['POST'])
+def update(index):
+  name = request.form.get('name')
+  email = request.form.get('email')  
+  phone = request.form.get('phone')
+  contacts[index]['name'] = name
+  contacts[index]['email'] = email
+  contacts[index]['phone'] = phone
+  return redirect('/')
+
+# IMPORTANTE 
 if __name__ == '__main__':
-    db.create_all()
-    app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080)
